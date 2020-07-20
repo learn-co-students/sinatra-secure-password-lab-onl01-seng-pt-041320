64f26dcdae9c459f86616d86c214e533acd9f868
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-19 19:38:33 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
     user = User.new(:username => params[:username], :password => params[:password])
 
     if user.save
-      redirect '/login'  
+      redirect '/account'  
     else
       redirect '/failure'
     end
